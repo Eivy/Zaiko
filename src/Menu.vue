921704d6c2b06124f6ab8acfa4140b4192d4c472
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-03-08 22:18:26 +0900

    Use router-link instead of a tag

--- src/Menu.vue
+++ src/Menu.vue
@@ -1,18 +1,8 @@
 <template>
-  <nav class="mdl-navigation">
-    <a class="mdl-navigation__link" @click='goto("sales")'><i class='material-icons'>store</i> 販売</a>
-    <a class="mdl-navigation__link" @click='goto("inventory")'><i class='material-icons'>view_module</i> 在庫</a>
+  <nav class='mdl-navigation'>
+    <router-link class='mdl-navigation__link' to='/sales'><i class='material-icons'>store</i> 販売</router-link>
+    <router-link class='mdl-navigation__link' to='/inventory'><i class='material-icons'>view_module</i> 在庫</router-link>
     <hr/>
-    <a class="mdl-navigation__link" @click='goto("settings")'><i class='material-icons'>settings</i> 管理</a>
+    <router-link class='mdl-navigation__link' to='/settings'><i class='material-icons'>settings</i> 管理</router-link>
   </nav>
 </template>
-
-<script>
-export default {
-  methods: {
-    goto: function (target) {
-      this.$router.push(target)
-    }
-  }
-}
-</script>
