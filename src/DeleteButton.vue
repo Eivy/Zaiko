e3ba0db02f24feee10ad13e2d583afa4695c44ad
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-03-23 11:56:45 +0900

    Add ripple effect to buttons

--- src/DeleteButton.vue
+++ src/DeleteButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click.stop=confirm class='mdl-button mdl-js-button mdl-button--raised mdl-button--accent'><Icon>delete_forever</Icon></button>
+  <button @click.stop=confirm class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent'><Icon>delete_forever</Icon></button>
 </template>
 
 <script>
--- src/SubmitButton.vue
+++ src/SubmitButton.vue
@@ -1,3 +1,3 @@
 <template>
-  <button class='mdl-button mdl-js-button mdl-button--raised mdl-button--colored'><i class='material-icons'>done</i></button>
+  <button class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored'><i class='material-icons'>done</i></button>
 </template>
