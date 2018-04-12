fd5baa511c418551d71fa044035c67dbb5113871
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-04-12 16:45:37 +0900

    Enable to logout

--- src/Menu.vue
+++ src/Menu.vue
@@ -4,5 +4,23 @@
     <router-link class='mdl-navigation__link' to='/inventory'><Icon>view_module</Icon> 在庫</router-link>
     <hr/>
     <router-link class='mdl-navigation__link' to='/settings/items'><Icon>settings</Icon> 管理</router-link>
+    <a @click='logout()' id='logout' class='mdl-navigation__link'><Icon>directions_run</Icon> ログアウト</a>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      firebase.auth().signOut()
+      location.reload()
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+#logout {
+  cursor: pointer;
+}
+</style>
