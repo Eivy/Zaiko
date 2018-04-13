ae73edd17c217b4a77155ee0fb6f5881c4a31f27
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-04-13 14:30:00 +0900

    Enable to signin with Github

--- src/Auth.vue
+++ src/Auth.vue
@@ -19,6 +19,7 @@ export default {
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
       tosUrl: ''
