d987d0415034e64d10e7cbd88755252e7830a4d3
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-04-17 15:32:15 +0900

    Enable to hide menu with click menu item

--- src/Menu.vue
+++ src/Menu.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class='mdl-navigation'>
-    <router-link class='mdl-navigation__link' to='/sales'><Icon>store</Icon> 販売</router-link>
-    <router-link class='mdl-navigation__link' to='/inventory'><Icon>view_module</Icon> 在庫</router-link>
+    <router-link class='mdl-navigation__link' @click.native='hide()' to='/sales'><Icon>store</Icon> 販売</router-link>
+    <router-link class='mdl-navigation__link' @click.native='hide()' to='/inventory'><Icon>view_module</Icon> 在庫</router-link>
     <hr/>
-    <router-link class='mdl-navigation__link' to='/settings/items'><Icon>settings</Icon> 管理</router-link>
+    <router-link class='mdl-navigation__link' @click.native='hide()' to='/settings/items'><Icon>settings</Icon> 管理</router-link>
     <a @click='logout()' id='logout' class='mdl-navigation__link'><Icon>directions_run</Icon> ログアウト</a>
   </nav>
 </template>
@@ -14,6 +14,9 @@ export default {
     logout () {
       firebase.auth().signOut()
       location.reload()
+    },
+    hide () {
+      document.querySelectorAll('.is-visible').forEach(e => e.classList.remove('is-visible'))
     }
   }
 }
