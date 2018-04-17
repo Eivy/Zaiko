35d21fca60c73cc75622eddf36971ac83ebb1ac7
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-04-17 11:05:54 +0900

    Fix for switching webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
-var path = require('path')
+const path = require('path')
 
-module.exports = [
-  {
-    entry: {
-      'index': path.join(__dirname, 'src', 'index.js')
-    },
-    output: {
-      path: path.join(__dirname, 'public'),
-      filename: '[name].js'
-    },
-    devtool: process.env.NODE_ENV === 'development' ? 'inline-source-map' : false,
-    module: {
-      rules: [
-        {
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          exclude: /node_modules/,
-          loader: 'eslint-loader',
-          options: {
-            formatter: require('eslint-friendly-formatter')
+module.exports = function (env, arg) {
+  return [
+    {
+      entry: {
+        'index': path.join(__dirname, 'src', 'index.js')
+      },
+      output: {
+        path: path.join(__dirname, 'public'),
+        filename: '[name].js'
+      },
+      devtool: arg.mode === 'development' ? 'inline-source-map' : false,
+      module: {
+        rules: [
+          {
+            enforce: 'pre',
+            test: /\.(js|vue)$/,
+            exclude: /node_modules/,
+            loader: 'eslint-loader',
+            options: {
+              formatter: require('eslint-friendly-formatter')
+            }
+          },
+          {
+            test: /\.vue$/,
+            exclude: /node_modules/,
+            loader: 'vue-loader'
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
           }
-        },
-        {
-          test: /\.vue$/,
-          exclude: /node_modules/,
-          loader: 'vue-loader'
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        }
-      ]
+        ]
+      }
     }
-  }
-]
+  ]
+}
